0d870576ae9e5365f3af1d4888d25611e4a313d9
nusquama <nusquama@gmail.com>
2024-11-02 09:15:16 +0100

    Update bindings.sh

--- bindings.sh
+++ bindings.sh
@@ -2,15 +2,26 @@
 
 bindings=""
 
-while IFS= read -r line || [ -n "$line" ]; do
-  if [[ ! "$line" =~ ^# ]] && [[ -n "$line" ]]; then
-    name=$(echo "$line" | cut -d '=' -f 1)
-    value=$(echo "$line" | cut -d '=' -f 2-)
-    value=$(echo $value | sed 's/^"\(.*\)"$/\1/')
-    bindings+="--binding ${name}=${value} "
-  fi
-done < .env.local
+# Vérifier si .env.local existe
+if [ -f .env.local ]; then
+  echo "Chargement des variables d'environnement depuis .env.local"
+  
+  # Lire le fichier .env.local et créer les bindings
+  while IFS= read -r line || [ -n "$line" ]; do
+    if [[ ! "$line" =~ ^# ]] && [[ -n "$line" ]]; then
+      name=$(echo "$line" | cut -d '=' -f 1)
+      value=$(echo "$line" | cut -d '=' -f 2-)
+      value=$(echo "$value" | sed 's/^"\(.*\)"$/\1/')  # Supprimer les guillemets autour de la valeur
+      bindings+="--binding ${name}=${value} "
+    fi
+  done < .env.local
 
-bindings=$(echo $bindings | sed 's/[[:space:]]*$//')
+else
+  echo ".env.local non trouvé. Utilisation des variables d'environnement de Coolify."
+fi
 
-echo $bindings
+# Supprimer les espaces superflus en fin de chaîne
+bindings=$(echo "$bindings" | sed 's/[[:space:]]*$//')
+
+# Afficher les bindings pour vérification
+echo "$bindings"
